290d1ba33e730351130cd64ba9a6966b80ddcea8
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-22 15:48:44 +0200

    Access reserve state using react useState

--- src/components/Rock.js
+++ src/components/Rock.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Rock = (props) => {
+  const [reserve, setReserve] = useState(false);
+  const handleReserve = () => {
+    setReserve(() => !reserve);
+  };
   const rocket = props;
   return (
-    <div>
-      <h1>{rocket.rock.rocket_name}</h1>
-      <p>{rocket.rock.description}</p>
-      <img src={rocket.rock.flickr_images[0]} alt={rocket.rocket_name} />
+    <div className="wrapper">
+      <div className="image">
+        <img src={rocket.rock.flickr_images[0]} alt={rocket.rocket_name} className="rocketImage" />
+      </div>
+      <div className="details">
+        <h1>{rocket.rock.rocket_name}</h1>
+        <p>
+          {reserve && <small>Reserved</small>}
+          {rocket.rock.description}
+        </p>
+        <button onClick={handleReserve} type="button">{reserve ? 'cancel Reservation' : 'Reserve Rocket'}</button>
+      </div>
+
     </div>
   );
 };
