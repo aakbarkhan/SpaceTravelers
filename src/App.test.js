7d7332c5fdd3e4460b7820f0bec2eeba4f70e924
aakbarkhan <aakbarkhan@gmail.com>
2021-12-23 20:26:10 +0530

    Fix linters

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store/configureStore';
-import Myprofile from './components/Myprofile';
 
 afterEach(() => cleanup());
 
