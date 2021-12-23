664888034568f5df2bb084401fbb72b0607295a5
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-23 15:50:20 +0200

    display mission and rockets in my profile components

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Myprofile = () => (
-  <div>
-    <h1>My profile components</h1>
-  </div>
-);
+const Myprofile = () => {
+  const rockets = useSelector((state) => state.rocketReducer);
+  const reservedItems = rockets.rockets.filter((item) => item.reserve === true);
+  const missionItems = rockets.missions.filter((item) => item.join === true);
+
+  return (
+    <div className="wrapper_profile">
+      <div className="profile">
+        <h3>My Rockets</h3>
+        <ul className="list">
+          {reservedItems.map((rock) => <li key={rock.rocket_name}>{rock.rocket_name}</li>)}
+        </ul>
+      </div>
+      <div className="profile">
+        <h3>My Missions</h3>
+        <ul className="list">
+          {
+            missionItems.map((mission) => (
+              <li key={mission.mission_name}>{mission.mission_name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Myprofile;
